390a930f71fdc959bff0edf1d341dfc4ef48569e
davydes <davydes@gmail.com>
2016-11-22 16:58:48 +0300

    try to repair CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,15 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift File.expand_path('../../spec/support', __FILE__)
 require 'yandex_kassa_form'
-require 'pry'
 
 PASSWORD_GOOD    = 's<kY23653f,{9fcnshwq'
 PASSWORD_INVALID = 'invalid pwd'
 PASSWORD         = PASSWORD_GOOD
 ERROR_MESSAGE    = 'message when errored'
-BODY_FILE        = 'generic_body.txt'     
+BODY_FILE        = 'generic_body.txt'
 TRUE_PROC        = Proc.new { [true] }
 
 def load_file(filename)
